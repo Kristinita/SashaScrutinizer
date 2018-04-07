06440b252873cdb2a2570f29b7e3dfef3d145ba7
Саша Черных <deploy@travis-ci.org>
2018-04-07 15:26:16 +0000

    Deploy PaletteMira to github.com/Kristinita/SashaScrutinizer.git:SashaDevelop

--- travis-install.sh
+++ travis-install.sh
@@ -1,10 +1,9 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-07 14:10:56
+# @Last Modified time: 2018-04-07 18:07:30
 parallel ::: 'pip install yamllint' \
              'pip install ruamel.yaml' \
              'npm install -g yamljs' \
              'pip install flake8' \
-             'pip install pydocstyle' \
-             'sudo apt-get install silversearcher-ag'
+             'pip install pydocstyle'
