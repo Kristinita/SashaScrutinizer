cc24859acf756047d9a91c82b0563358842f0d27
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-07 18:33:18 +0300

    ci(travis): Try move output results to folder

--- travis-update-package-managers.sh
+++
@@ -1,6 +0,0 @@
-# @Author: SashaChernykh
-# @Date:   2018-04-04 20:18:44
-# @Last Modified by:   SashaChernykh
-# @Last Modified time: 2018-04-04 20:21:39
-parallel ::: 'pip install -U pip' \
-			 'npm install --global npm'
