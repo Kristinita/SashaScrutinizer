e3f22c2dda4dd0500749d81daeb2b8bcbf921331
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-07 14:11:24 +0300

    ci(travis): Try sudo for silver_searcher installation

--- travis-install.sh
+++ travis-install.sh
@@ -1,10 +1,10 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-07 14:06:53
+# @Last Modified time: 2018-04-07 14:10:56
 parallel ::: 'pip install yamllint' \
              'pip install ruamel.yaml' \
              'npm install -g yamljs' \
              'pip install flake8' \
              'pip install pydocstyle' \
-             'apt-get install silversearcher-ag'
+             'sudo apt-get install silversearcher-ag'
