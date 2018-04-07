8f818bf591276e9826abbd4ec9639bbd73c3c423
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-07 18:05:16 +0300

    ci(travis): Try build silver_searcher from source

--- travis-install.sh
+++ travis-install.sh
@@ -1,10 +1,10 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-07 18:01:37
+# @Last Modified time: 2018-04-07 18:04:16
 parallel ::: 'pip install yamllint' \
              'pip install ruamel.yaml' \
              'npm install -g yamljs' \
              'pip install flake8' \
              'pip install pydocstyle' \
-             'apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev'
+             'sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev && ./build.sh && sudo make install'
