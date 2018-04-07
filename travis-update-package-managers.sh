2ce4d40e96f54a3b94e5e580d8a32f925f1d29fc
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-07 14:08:32 +0300

    ci(travis): Try silver_searcher

---
+++ travis-update-package-managers.sh
@@ -0,0 +1,6 @@
+# @Author: SashaChernykh
+# @Date:   2018-04-04 20:18:44
+# @Last Modified by:   SashaChernykh
+# @Last Modified time: 2018-04-04 20:21:39
+parallel ::: 'pip install -U pip' \
+			 'npm install --global npm'
