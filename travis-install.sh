df7506e87a3628bce675c2eeff1185e2f1f43086
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-07 18:07:59 +0300

    ci(travis): Try step-by-ste build process

--- travis-install.sh
+++ travis-install.sh
@@ -1,10 +1,9 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-07 18:04:16
+# @Last Modified time: 2018-04-07 18:07:30
 parallel ::: 'pip install yamllint' \
              'pip install ruamel.yaml' \
              'npm install -g yamljs' \
              'pip install flake8' \
-             'pip install pydocstyle' \
-             'sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev && ./build.sh && sudo make install'
+             'pip install pydocstyle'
