11939390c882f09f6506131b2b1b24413c6db42a
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-07 18:02:16 +0300

    ci(travis): Try compile silver_searcher to Ubuntu

--- travis-install.sh
+++ travis-install.sh
@@ -1,10 +1,10 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-07 14:10:56
+# @Last Modified time: 2018-04-07 18:01:37
 parallel ::: 'pip install yamllint' \
              'pip install ruamel.yaml' \
              'npm install -g yamljs' \
              'pip install flake8' \
              'pip install pydocstyle' \
-             'sudo apt-get install silversearcher-ag'
+             'apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev'
