19f9b899c37fad5ae3b9830bc90e066c8b946729
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-08 16:16:02 +0300

    ci(travis): Try Jsonlint

--- travis-install.sh
+++ travis-install.sh
@@ -1,11 +1,12 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-08 12:20:35
+# @Last Modified time: 2018-04-08 16:14:40
 parallel ::: 'pip install yamllint' \
              'pip install ruamel.yaml' \
              'npm install -g yamljs' \
              'pip install flake8' \
              'pip install pydocstyle' \
              'gem install i18n_yaml_sorter' \
-             'pip install st-package-reviewer'
+             'pip install st-package-reviewer' \
+             'npm install -g jsonlint'
