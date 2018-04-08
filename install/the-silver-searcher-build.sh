465f9e8dac9a081e5abcb7bdb8c1ee946b4ecf2a
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-08 17:13:17 +0300

    ci(travis): Try only one command in parallel

--- install/the-silver-searcher-build.sh
+++
@@ -1,9 +0,0 @@
-# @Author: Kristinita
-# @Date:   2018-04-08 16:39:56
-# @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-08 16:41:04
-git clone https://github.com/ggreer/the_silver_searcher --depth=1
-cd the_silver_searcher
-sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
-bash ./build.sh
-sudo make install
--- install/travis-install.sh
+++ install/travis-install.sh
@@ -1,13 +1,5 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-08 17:06:33
-parallel ::: 'pip install yamllint' \
-             # 'pip install ruamel.yaml' \
-             # 'npm install -g yamljs' \
-             # 'pip install flake8' \
-             # 'pip install pydocstyle' \
-             # 'gem install i18n_yaml_sorter' \
-             # 'pip install st-package-reviewer' \
-             # 'npm install -g jsonlint-cli' \
-             'git clone https://github.com/ggreer/the_silver_searcher --depth=1 && cd the_silver_searcher && sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev && bash ./build.sh && sudo make install'
+# @Last Modified time: 2018-04-08 17:12:55
+parallel ::: 'git clone https://github.com/ggreer/the_silver_searcher --depth=1 && cd the_silver_searcher && sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev && bash ./build.sh && sudo make install'
