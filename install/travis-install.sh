7e8ed8c75265a46d310e2f78a77bf7cfc9cb94d9
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-08 17:00:29 +0300

    ci(travis): Add install to travis-install path

--- install/travis-install.sh
+++ install/travis-install.sh
@@ -1,7 +1,7 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-08 16:53:57
+# @Last Modified time: 2018-04-08 16:59:36
 parallel ::: 'pip install yamllint' \
              # 'pip install ruamel.yaml' \
              # 'npm install -g yamljs' \
@@ -10,4 +10,4 @@ parallel ::: 'pip install yamllint' \
              # 'gem install i18n_yaml_sorter' \
              # 'pip install st-package-reviewer' \
              # 'npm install -g jsonlint-cli' \
-             'bash ./the-silver-searcher-build.sh'
+             'bash install/the-silver-searcher-build.sh'
