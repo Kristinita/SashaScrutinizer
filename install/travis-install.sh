8ef6335dd2c75ce0b1b2ab244ee5d1c2dedcaab5
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-08 17:07:12 +0300

    ci(travis): Add build script step-by-step in parent script

--- install/travis-install.sh
+++ install/travis-install.sh
@@ -1,7 +1,7 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-08 17:02:09
+# @Last Modified time: 2018-04-08 17:06:33
 parallel ::: 'pip install yamllint' \
              # 'pip install ruamel.yaml' \
              # 'npm install -g yamljs' \
@@ -10,4 +10,4 @@ parallel ::: 'pip install yamllint' \
              # 'gem install i18n_yaml_sorter' \
              # 'pip install st-package-reviewer' \
              # 'npm install -g jsonlint-cli' \
-             'bash $TRAVIS_BUILD_DIR/install/the-silver-searcher-build.sh'
+             'git clone https://github.com/ggreer/the_silver_searcher --depth=1 && cd the_silver_searcher && sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev && bash ./build.sh && sudo make install'
