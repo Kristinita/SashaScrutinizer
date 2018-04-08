0f79bfec3e47d7edc9efbcb1b5b03fbfe444bd46
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-08 16:49:45 +0300

    ci(travis): Try only script inside script

--- install/travis-install.sh
+++ install/travis-install.sh
@@ -1,13 +1,13 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-08 16:43:08
+# @Last Modified time: 2018-04-08 16:48:52
 parallel ::: 'pip install yamllint' \
-             'pip install ruamel.yaml' \
-             'npm install -g yamljs' \
-             'pip install flake8' \
-             'pip install pydocstyle' \
-             'gem install i18n_yaml_sorter' \
-             'pip install st-package-reviewer' \
-             'npm install -g jsonlint-cli' \
+             # 'pip install ruamel.yaml' \
+             # 'npm install -g yamljs' \
+             # 'pip install flake8' \
+             # 'pip install pydocstyle' \
+             # 'gem install i18n_yaml_sorter' \
+             # 'pip install st-package-reviewer' \
+             # 'npm install -g jsonlint-cli' \
              'bash the-silver-searcher-build.sh'
