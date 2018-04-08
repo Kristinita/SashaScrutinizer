d08471f7b1cc18b8731522e5eccc567e0f33a028
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-08 19:41:45 +0300

    ci(travis): Try install Travis Client 2

--- install/travis-install.sh
+++ install/travis-install.sh
@@ -1,7 +1,7 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-08 17:26:20
+# @Last Modified time: 2018-04-08 19:41:06
 parallel ::: 'pip install yamllint' \
              'pip install ruamel.yaml' \
              'npm install -g yamljs' \
@@ -10,5 +10,6 @@ parallel ::: 'pip install yamllint' \
              'gem install i18n_yaml_sorter' \
              'pip install st-package-reviewer' \
              'npm install -g jsonlint-cli' \
-			 'git clone https://github.com/ggreer/the_silver_searcher --depth=1 && cd the_silver_searcher && sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev && bash ./build.sh && sudo make install'
+			 'git clone https://github.com/ggreer/the_silver_searcher --depth=1 && cd the_silver_searcher && sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev && bash ./build.sh && sudo make install' \
+			 'gem install travis'
 
