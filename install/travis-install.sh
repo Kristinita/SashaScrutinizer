1721572f3ff826316af6696640b2d42ab85b351c
Саша Черных <deploy@travis-ci.org>
2018-04-08 14:29:48 +0000

    Deploy PaletteMira to github.com/Kristinita/SashaScrutinizer.git:SashaDevelop

--- install/travis-install.sh
+++ install/travis-install.sh
@@ -1,7 +1,7 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-08 16:22:05
+# @Last Modified time: 2018-04-08 17:26:20
 parallel ::: 'pip install yamllint' \
              'pip install ruamel.yaml' \
              'npm install -g yamljs' \
@@ -9,4 +9,6 @@ parallel ::: 'pip install yamllint' \
              'pip install pydocstyle' \
              'gem install i18n_yaml_sorter' \
              'pip install st-package-reviewer' \
-             'npm install -g jsonlint-cli'
+             'npm install -g jsonlint-cli' \
+			 'git clone https://github.com/ggreer/the_silver_searcher --depth=1 && cd the_silver_searcher && sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev && bash ./build.sh && sudo make install'
+
