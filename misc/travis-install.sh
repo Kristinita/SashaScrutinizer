32fb3032b1413d41fe8bd2149627aad56758c671
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-23 21:15:14 +0300

    ci(travir): try install the_silver_searcher requirements via APT addon

--- misc/travis-install.sh
+++ misc/travis-install.sh
@@ -1,7 +1,7 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-23 17:25:57
+# @Last Modified time: 2018-04-23 21:12:57
 parallel ::: 'pip install yamllint' \
 	'pip install ruamel.yaml' \
 	'npm install -g yamljs' \
@@ -10,7 +10,7 @@ parallel ::: 'pip install yamllint' \
 	'gem install i18n_yaml_sorter' \
 	'pip install st-package-reviewer' \
 	'npm install -g jsonlint-cli' \
-	'git clone https://github.com/ggreer/the_silver_searcher --depth=1 && cd the_silver_searcher && sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev && bash ./build.sh && sudo make install' \
+	'git clone https://github.com/ggreer/the_silver_searcher --depth=1 && cd the_silver_searcher && bash ./build.sh && sudo make install' \
 	'gem install travis' \
 	'npm install -g @commitlint/travis-cli' \
 	'npm install -g eclint'
