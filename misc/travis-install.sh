1d984deefbe5e1498a2b683f64d67386ee2b1ef9
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-04-23 17:26:18 +0300

    ci(travis): try remove unexpected slash

--- misc/travis-install.sh
+++ misc/travis-install.sh
@@ -1,7 +1,7 @@
 # @Author: SashaChernykh
 # @Date:   2018-04-04 20:18:44
 # @Last Modified by:   Kristinita
-# @Last Modified time: 2018-04-23 17:12:52
+# @Last Modified time: 2018-04-23 17:25:57
 parallel ::: 'pip install yamllint' \
 	'pip install ruamel.yaml' \
 	'npm install -g yamljs' \
@@ -13,4 +13,4 @@ parallel ::: 'pip install yamllint' \
 	'git clone https://github.com/ggreer/the_silver_searcher --depth=1 && cd the_silver_searcher && sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev && bash ./build.sh && sudo make install' \
 	'gem install travis' \
 	'npm install -g @commitlint/travis-cli' \
-	'npm install -g eclint' \
+	'npm install -g eclint'
